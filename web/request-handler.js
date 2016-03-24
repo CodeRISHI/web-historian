4cc3f189995dfb3e883216a4696fce381d569c0b
CodeRISHI <arunv83@gmail.com>
2016-03-23 19:08:03 -0700

    Return content from index.html

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,30 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var httpHelpers = require('./http-helpers');
+
+var fs = require('fs');
+var request = require('request');
+var indexUrl = '/Users/student/Codes/2016-02-web-historian/web/public/index.html';
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  var statusCode = 200;
+  if (req.url === '/') {
+
+    // request(indexUrl, function());
+
+    var indexFile = fs.readFile(indexUrl, 'utf8', function(err, content) {
+      if (err) {
+        console.log(err);
+      } else {
+        // console.log(content, typeof content);
+        res.writeHead(statusCode, httpHelpers.headers);
+        res.end(content);
+
+      }
+    });
+    
+  }
+  // res.end(archive.paths.list);
+
 };
